10eaf04b49838d986809eb66b63c45df053c11c1
ibrahim-lombarkia <168268896+ibrahim-lombarkia@users.noreply.github.com>
2025-05-29 02:18:36 +0100

    Add files via upload

---
+++ home.php
@@ -0,0 +1,333 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> YourPerfectTrip </title>
+
+    <link rel="stylesheet" href="./css/style.css">
+    </head>
+<body>
+
+<header>
+
+    <input type="checkbox" name="" id="toggler">
+    <label for="toggler" class="fas fa-bars"></label>
+
+    <a href="#" class="logo"><span>Your Perfect Trip</span></a>
+
+    <nav class="navbar">
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#products">Places</a>
+        <a href="#review">Review</a>
+        <a href="./Login/login.php">Login/Signup</a>
+        <a href="./admin/adminlogin.php">Admin</a>
+    </nav>
+
+
+</header>
+
+<section class="home" id="home">
+
+    <div class="content">
+        <span style="color:Black;"> Welcome to</span><span style="color:Green;"> Al</span><span style="color:red;">ger</span><span style="color:white;">ia: </span>
+        <p>The country of one and a half million martyrs.</p>
+        <a href="#products" class="btn">Travel Now</a>
+    </div>
+
+</section>
+
+
+<section class="about" id="about">
+
+    <h1 class="heading"> <span> about </span> us </h1>
+
+    <div class="row">
+
+        <div class="video-container">
+            <video src="./images/about-vid.mp4" loop autoplay muted></video>
+            
+        </div>
+
+        <div class="content">
+            <h3>why choose us?</h3>
+<p> What truly sets us apart is our unwavering commitment to your satisfaction. Our 24/7 customer support is always ready to assist you, and we offer competitive prices and exclusive deals to save you money while you explore the world. Choose our travel website, and you're not just a traveler; you're part of a community that values your journey and strives to make it enjoyable and hassle-free. Join us today for unforgettable adventures, where your travel needs take center stage.</p>
+            <a href="#review" class="btn">learn more</a>
+        </div>
+
+    </div>
+
+</section> 
+
+<section class="icons-container">
+
+    <div class="icons">
+        <img src="./images/icon-1.png" alt="">
+        <div class="info">
+            <h3>free booking</h3>
+            <span>on all orders</span>
+        </div>
+    </div>
+
+    <div class="icons">
+        <img src="./images/icon-2.png" alt="">
+        <div class="info">
+            <h3>10 days returns</h3>
+            <span>moneyback guarantee</span>
+        </div>
+    </div>
+
+    <div class="icons">
+        <img src="./images/icon-3.png" alt="">
+        <div class="info">
+            <h3>offer & gifts</h3>
+            <span>on all orders</span>
+        </div>
+    </div>
+
+    <div class="icons">
+        <img src="./images/icon-4.png" alt="">
+        <div class="info">
+            <h3>secure paymens</h3>
+            <span>protected by Razorpay</span>
+        </div>
+    </div>
+
+</section>
+
+
+<section class="products" id="products">
+
+    <h1 class="heading"> Popular <span>Places</span> </h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <div class="image">
+                <img src="./images/Batna.jpg" alt="">
+                <div class="icons">
+                    <a href="./Login/login.php" class="cart-btn">Visit Us</a>
+                </div>
+            </div>
+            <div class="content">
+                <h3>Batna</h3>
+                <div class="price">40000 DA</div>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="./images/constantine.jpg" alt="">
+                <div class="icons">
+                    <a href="./Login/login.php" class="cart-btn">Visit Us</a>
+                </div>
+            </div>
+            <div class="content">
+                <h3>Constantine</h3>
+                <div class="price">35000 DA</div>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="./images/Algiers.jpg" alt="">
+                <div class="icons">
+                    <a href="./Login/login.php" class="cart-btn">Visit Us</a>
+                </div>
+            </div>
+            <div class="content">
+                <h3>Algiers</h3>
+                <div class="price">20000 DA</div>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="./images/Oran.jpg" alt="">
+                <div class="icons">
+                    <a href="./Login/login.php" class="cart-btn">Visit Us</a>
+                </div>
+            </div>
+            <div class="content">
+                <h3>Oran</h3>
+                <div class="price">25000 DA</div>
+            </div>
+        </div>
+
+
+        <div class="box">
+            <div class="image">
+                <img src="./images/Tamanrassett.jpg" alt="">
+                <div class="icons">
+                    <a href="./Login/login.php" class="cart-btn">Visit Us</a>
+                </div>
+            </div>
+            <div class="content">
+                <h3>Tamanrasset</h3>
+                <div class="price">80000 DA</div>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="./images/Sefar.jpg" alt="">
+                <div class="icons">
+                    <a href="./Login/login.php" class="cart-btn">Visit Us</a>
+                </div>
+            </div>
+            <div class="content">
+                <h3>Sefar</h3>
+                <div class="price">65000 DA</div>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="./images/HoggarMountains.jpg" alt="">
+                <div class="icons">
+                    <a href="./Login/login.php" class="cart-btn">Visit Us</a>
+                </div>
+            </div>
+            <div class="content">
+                <h3>Hoggar Mountains</h3>
+                <div class="price">75000 DA</div>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="./images/TassiliCaves.jpg" alt="">
+                <div class="icons">
+                    <a href="./Login/login.php" class="cart-btn">Visit Us</a>
+                </div>
+            </div>
+            <div class="content">
+                <h3>Tassili Caves</h3>
+                <div class="price">60000 DA</div>
+            </div>
+        </div>
+
+    </div>
+
+</section>
+
+
+<section class="review" id="review">
+
+<h1 class="heading"> customer's <span>review</span> </h1>
+
+<div class="box-container">
+
+    <div class="box">
+        <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+        </div>
+        <p>Booking my dream trip to the serene backwaters of Oran was a breeze with 'Your Perfect Trip' – seamless and stress-free! </p>
+        <div class="user">
+            <div class="user-info">
+                <h3>Atharva</h3>
+                <span>Goa</span>
+            </div>
+        </div>
+        <span class="fas fa-quote-right"></span>
+    </div>
+
+    <div class="box">
+        <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+        </div>
+        <p>Thanks to 'Your Perfect Trip', I explored the majestic beauty of Batna with ease, and the deals were unbeatable!</p>
+        <div class="user">
+            
+            <div class="user-info">
+                <h3>Pretty</h3>
+                <span>Maharashtra</span>
+            </div>
+        </div>
+        <span class="fas fa-quote-right"></span>
+    </div>
+
+    <div class="box">
+        <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+        </div>
+        <p>Made my adventure to the mesmerizing Hoggar Mountains unforgettable, with expert guidance and fantastic itineraries</p>
+        <div class="user">
+            
+            <div class="user-info">
+                <h3>Tejas</h3>
+                <span>Kerala</span>
+            </div>
+        </div>
+        <span class="fas fa-quote-right"></span>
+    </div>
+
+</div>
+
+</section>
+
+
+<section class="contact" id="contact">
+
+    <h1 class="heading"> <span> contact </span> us </h1>
+
+    <div class="row">
+
+        <form action="">
+            <input type="text" placeholder="name" class="box">
+            <input type="email" placeholder="email" class="box">
+            <input type="number" placeholder="number" class="box">
+            <textarea name="" class="box" placeholder="message" id="" cols="30" rows="10"></textarea>
+            <input type="submit" value="send message" class="btn">
+        </form>
+
+        <div class="image">
+            <img src="./images/contact-img.svg" alt="">
+        </div>
+
+    </div>
+
+</section>
+
+
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="#">Home</a>
+            <a href="#about">About </a>
+            <a href="#review">Review</a>
+            <a href="#contact">Contact Us</a>
+        </div>
+
+        <div class="box">
+            <h3>Popular Travel Locations</h3>
+            <a href="#products">Algiers</a>
+            <a href="#products">Oran</a>
+            <a href="#products">Constantine</a>
+            <a href="#products">Batna</a>
+        </div>
+    </div>
+
+</section>
+
+
+
+</body>
+</html>
